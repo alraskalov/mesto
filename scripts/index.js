a6e02408ede1ec27ba325e8799dc86f18d927bd8
Alexander Dolzhenko <raskalovalexander@yandex.ru>
2021-10-01 01:39:19 +0300

    refactor: unused code has been removed

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,3 @@
-// const popup = document.querySelector(".popup");
-
-// let formElement = popup.querySelector(".popup__edit-form");
-
-// let nameInput = formElement.querySelector("#userName");
-// let jobInput = formElement.querySelector("#userJob");
-// let popupOpen = document.querySelector(".popup-open");
-// let popupClose = formElement.querySelector(".popup__close-btn");
-// let userName = document.querySelector(".profile__title");
-// let userProfession = document.querySelector(".profile__subtitle");
-
-// function switchPopup () {
-//   popup.classList.toggle("popup_opened");
-//   nameInput.value = userName.textContent;
-//   jobInput.value = userProfession.textContent;
-// }
-
-// function handlerFormSubmit(evt) {
-//   evt.preventDefault();
-
-//   userName.textContent = nameInput.value;
-//   userProfession.textContent = jobInput.value;
-//   switchPopup();
-// }
-
-// popupOpen.addEventListener("click", switchPopup);
-// popupClose.addEventListener("click", switchPopup);
-// formElement.addEventListener("submit", handlerFormSubmit);
-
 const initialCards = [
   {
     name: "Архыз",
